903373afa0e78a3d2ccee2df2899644157a5e567
Chris5477 <petitcolaschristopher@gmail.com>
2022-01-02 20:11:51 +0100

    add listEmployee page and fix error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import CreateEmployee from "./pages/CreateEmployee";
+import ListEmployee from "./pages/ListEmployee";
 
 const App = () => {
 	return (
 		<div className="App">
 			<CreateEmployee />
+			<ListEmployee />
 		</div>
 	);
 };
---
+++ src/pages/ListEmployee.js
@@ -0,0 +1,10 @@
+const ListEmployee = () => {
+	return (
+		<div id="employee-div" class="container">
+			<h2>Current Employees</h2>
+			<table id="employee-table" class="display"></table>
+			<a href="index.html">Home</a>
+		</div>
+	);
+};
+export default ListEmployee;
