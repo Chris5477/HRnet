6f064a7f43da4365e0e1784292436fcd5986cb2a
Chris5477 <petitcolaschristopher@gmail.com>
2022-01-10 10:50:06 +0100

    add calendar

---
+++ src/components/Datapicker.js
@@ -0,0 +1,11 @@
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+const Datepicker = () => {
+	return (
+		<div className="calendar no-display ">
+			<Calendar />
+		</div>
+	);
+};
+export default Datepicker;
--- src/components/InputWrapper.js
+++ src/components/InputWrapper.js
@@ -2,20 +2,38 @@ import { options } from "../ListingComponents/Listing-option";
 import Option from "./Option";
 
 const InputWrapper = ({ label, text }) => {
-	return label == "Department" ? (
-		<div className="formData">
-			<label htmlFor={label}>Department</label>
-			<select name={label} id={label}>
-				{options.map(({ value, text }, index) => (
-					<Option value={value} text={text} key={`index ${index}`} />
-				))}
-			</select>
-		</div>
-	) : (
-		<div className="formData">
-			<label htmlFor={label}>{text}</label>
-			<input id={label} type="text" />
-		</div>
-	);
+	const showCalendar = () => {
+		document.querySelector(".calendar").classList.remove("no-display");
+	};
+	let input = null;
+
+	if (label == "Department") {
+		input = (
+			<div className="formData">
+				<label htmlFor={label}>Department</label>
+				<select name={label} id={label}>
+					{options.map(({ value, text }, index) => (
+						<Option value={value} text={text} key={`index ${index}`} />
+					))}
+				</select>
+			</div>
+		);
+	} else if (label == "date-of-birth" || label == "start-date") {
+		input = (
+			<div className="formData">
+				<label htmlFor={label}>{text}</label>
+				<input onFocus={showCalendar} id={label} type="text" />
+			</div>
+		);
+	} else {
+		input = (
+			<div className="formData">
+				<label htmlFor={label}>{text}</label>
+				<input id={label} type="text" />
+			</div>
+		);
+	}
+
+	return input;
 };
 export default InputWrapper;
