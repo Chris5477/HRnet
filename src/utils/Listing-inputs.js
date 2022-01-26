b10e082302727a987720fa5539f36380b1f0fd97
Chris5477 <petitcolaschristopher@gmail.com>
2022-01-26 16:02:31 +0100

    improve code to create employee

--- src/utils/Listing-inputs.js
+++ src/utils/Listing-inputs.js
@@ -1,19 +1,23 @@
 export const arrayInputsIdentity = [
 	{
-		label: "first-name",
+		label: "firstName",
 		text: "First Name",
+		value: "",
 	},
 	{
-		label: "last-name",
+		label: "lastName",
 		text: "Last Name",
+		value: "",
 	},
 	{
 		label: "date-of-birth",
 		text: "Date of Birth",
+		value: "",
 	},
 	{
 		label: "start-date",
 		text: "Start Date",
+		value: "",
 	},
 ];
 
@@ -21,19 +25,23 @@ export const arrayInputsAdress = [
 	{
 		label: "street",
 		text: "Street",
+		value: "",
 	},
 
 	{
 		label: "city",
 		text: "City",
+		value: "",
 	},
 
 	{
 		label: "state",
 		text: "State",
+		value: "",
 	},
 	{
 		label: "zip-code",
 		text: "Zip Code",
+		value: "",
 	},
 ];
